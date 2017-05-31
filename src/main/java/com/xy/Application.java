34cf4b5e61e5e7716ecfabd2d71995071926761c
熊焱 <xiongyan@staff.hexun.com>
2017-05-31 16:00:09 +0800

    session

--- src/main/java/com/xy/Application.java
+++ src/main/java/com/xy/Application.java
@@ -2,7 +2,10 @@ package com.xy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportResource;
 
 /**
@@ -15,6 +18,16 @@ import org.springframework.context.annotation.ImportResource;
 @SpringBootApplication
 @ImportResource("classpath:/spring/applicationContext-disconf.xml")
 public class Application {
+	
+	@Bean
+	public EmbeddedServletContainerCustomizer containerCustomizer() {
+		return new EmbeddedServletContainerCustomizer() {
+			@Override
+			public void customize(ConfigurableEmbeddedServletContainer container) {
+				container.setSessionTimeout(86400); // 一天
+			}
+		};
+	}
 	
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
